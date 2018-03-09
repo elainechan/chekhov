506939d44548aaa040c5906d138bb9a0adcec5f2
elainechan <elaine.chan@outlook.com>
2018-03-09 13:59:33 -0500

    Clarifies language in example.

--- solve.py
+++ solve.py
@@ -1,54 +1,90 @@
 """
-Two tasks
+N tasks
 	Each task consists of several subtasks
 	Each subtask must be done in order
 Two workers, Alice and Bob
 	Each worker has four 30-min time slots
 	Each subtask must be done by a specific worker
 
+Example task list: 
 task1 = ["A1", "B2", "A1", "B3"]
 task2 = ["B2, "A2", "B4", "A3"]
 task3 = ["B1", "A3", "B1", "A1"]
 task4 = ["A4", "B1", "A1", "B1"]
 
-Example even time:
-1A1 4A4
-3B1 1B2 2B1
+Where in task1 "A1",
+"A" stands for "assigned to Alice"
+and "1" means the task takes 1 hour to complete. 
 
-Convention: pick shortest tasks first
+Example schedule where each worker puts in equal time:
+Alice: 1A1 4A4
+Bob: 3B1 1B2 2B1
 
-A = "1A1" => position 1 hours total
-B = "3B1" => position 1 hours total
+Let's solve manually.
 
-A = "3A3" => position 4 hours total
-B = "2B2" => position 3 hours total
+First round:
+task1 = ["A1", "B2", "A1", "B3"]
+task2 = ["B2, "A2", "B4", "A3"]
+task3 = ["B1", "A3", "B1", "A1"]
+task4 = ["A4", "B1", "A1", "B1"]
+A = "1A1" => position 1 (has worked 1 hour)
+B = "3B1" => position 1
 
-task1 = ["1B2", "1A1", "1B3"]
-task2 = ["2B2, "2A2", "2B4", "2A3"]
-task3 = ["3B1", "3A1"]
-task4 = ["4A4", "4B1", "4A1", "4B1"]
+Second round:
+task1 = ["B2", "A1", "B3"]
+task2 = ["B2, "A2", "B4", "A3"]
+task3 = ["A3", "B1", "A1"]
+task4 = ["A4", "B1", "A1", "B1"]
+A = "3A3" => position 4 (has worked 4 hours cumulative)
+B = "2B2" => position 3
 
+Third round:
+task1 = ["B2", "A1", "B3"]
+task2 = ["A2", "B4", "A3"]
+task3 = ["B1", "A1"]
+task4 = ["A4", "B1", "A1", "B1"]
 A = "3A1" => 5
 B = "3B1" => 4
 
+Fourth round:
 task1 = ["B2", "A1", "B3"]
-task2 = ["B2, "A2", "B4", "A3"]
+task2 = ["A2", "B4", "A3"]
 task3 = []
 task4 = ["A4", "B1", "A1", "B1"]
-
 A = "1A1" => 6
 B = "1B2" => 6
 
+Fifth round:
+task1 = ["B3"]
+task2 = ["A2", "B4", "A3"]
+task3 = []
+task4 = ["A4", "B1", "A1", "B1"]
 A = "2A2" => 8
 B = "1B3" => 9
 
+Sixth round:
+task1 = []
+task2 = ["B4", "A3"]
+task3 = []
+task4 = ["A4", "B1", "A1", "B1"]
 A = "4A4" => 12
 B = "2B4" => 13
 
+Seventh round:
+task1 = []
+task2 = ["A3"]
+task3 = []
+task4 = ["B1", "A1", "B1"]
 A = "2A3" => 15
 B = "4B1" => 14
 
+Eigth round:
+task1 = []
+task2 = []
+task3 = []
+task4 = ["A1"]
 A = "4A1" => 16
+B = "" => 14
 """
 tasklist = [
 	["A1", "B2", "A1", "B3"],
