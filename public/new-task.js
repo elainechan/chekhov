fb92ccf60a538cd50c4389c7ed71091f74c67f83
elainechan <elaine.chan@outlook.com>
2018-06-18 16:24:58 -0400

    Post from task page.

--- public/new-task.js
+++ public/new-task.js
@@ -7,7 +7,7 @@ function getCaseData(callback) {
 function createCaseSelection(CASES) {
 	console.log(CASES);
 	CASES.forEach((data => {
-		$("#case-selection").append(`<option value="${data._id}">${data.name}</option>`);
+		$("#case-selection").append(`<option class="case-selected" value="${data._id}">${data.name}</option>`);
 	}));
 }
 
@@ -16,8 +16,13 @@ function postNewTask() {
 		e.preventDefault();
 		/* configure the json of request */
 		console.log($('#task-name').val());
+		let caseObjId = $('#case-selection option:selected', this).data('value');
+		console.log(caseObjId);
+		let caseIdStr = JSON.stringify(caseObjId);
 		let taskObj = {
-			name: $('#task-name').val()
+			name: $('#task-name').val(),
+			description: $('#task-description').val(),
+			caseId: caseIdStr
 		};
 		$.ajax({
 			url: `http://localhost:8080/tasks/${localStorage.getItem('token')}`,
